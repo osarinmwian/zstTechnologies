0b1ed72fa8fe6fc118053d8f0c4b1f1d65589d4c
osarinmwian <noel.osarinmwian@gmail.com>
2023-04-09 03:51:10 +0100

    install dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@app': ['./src'],
+            tests: ['./ tests'],
+            '@assets': ['./assets'],
+            '@themes': ['./assets/themes/index.ts'],
+            '@components': ['./src/components'],
+            '@constants': ['./src/constants/index.ts'],
+            '@hook': ['./src/hook/index.ts'],
+            '@navigation': ['./src/navigation'],
+            '@screens': ['./src/screens'],
+            '@store': ['./src/store'],
+            '@utils': ['./src/utils/index.ts'],
+          },
+        },
+      ],
+
+    ],
   };
 };
