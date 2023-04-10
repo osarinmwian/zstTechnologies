48b80520693bffc3097687fdf65c1615002243b4
osarinmwian <noel.osarinmwian@gmail.com>
2023-04-10 14:08:59 +0100

    add redux

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import RootNavigator from "@app/navigation/root_navigator";
 import TopNavigation from "@app/navigation/top_navigator";
+import store from "@app/redux/store";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <RootNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 }
